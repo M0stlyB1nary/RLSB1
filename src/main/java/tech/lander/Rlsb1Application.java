229cbf8c7b60ba02d5bb8045f59104aa15f08c91
Rory Lander <idigchili@gmail.com>
2016-05-20 23:55:18 -0500

    Original check in with working code.

---
+++ src/main/java/tech/lander/Rlsb1Application.java
@@ -0,0 +1,12 @@
+package tech.lander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rlsb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rlsb1Application.class, args);
+	}
+}
---
+++ src/test/java/tech/lander/Rlsb1ApplicationTests.java
@@ -0,0 +1,16 @@
+package tech.lander;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Rlsb1Application.class)
+public class Rlsb1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
